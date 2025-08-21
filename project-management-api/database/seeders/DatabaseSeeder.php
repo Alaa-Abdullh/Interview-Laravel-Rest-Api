30d0e684eae0f0f3405c5db70feae350b92057f1
Alaa Abdullah <alaaabdullh43@gmail.com>
2025-08-21 23:02:05 +0300

    seed users, projects, and tasks in DatabaseSeeder

--- project-management-api/database/seeders/DatabaseSeeder.php
+++ project-management-api/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\project;
+use App\Models\Task;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +18,25 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       $users= User::factory(12)->create();
+       $projects= Project::factory(5)->create();
+    //    $tasks= Task::factory()->count(30)->create()
+    //    ->each(function ($task) use ($users) {
+    //     $task->users()->sync(
+    //         $users->random(rand(1, 5))->pluck('id')->toArray()
+    //     );      
+        
+    //    });
+
+    //or 
+      $projects->each(function($project)use($users){
+          Task::factory(30)->create(['project_id' => $project->id])
+            ->each(function ($task) use ($users) {
+                $task->users()->sync(
+                    $users->random(rand(1, 5))->pluck('id')->toArray()
+                );
+            });
+      });
+
     }
 }
