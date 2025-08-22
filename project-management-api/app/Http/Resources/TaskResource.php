ebc748da3c7378568ca0f87c30618e1b00becb3d
Alaa Abdullah <alaaabdullh43@gmail.com>
2025-08-23 00:35:54 +0300

    Add API Resources for Project, Task, and User

---
+++ project-management-api/app/Http/Resources/TaskResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'project_id' => $this->project_id,
+            'title' => $this->title,
+            'details' => $this->details,
+            'priority' => $this->priority,
+            'is_completed' => (bool) $this->is_completed,
+            'assignee_ids' => UserResource::collection($this->whenLoaded('users')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+        ];
+    }
+}
